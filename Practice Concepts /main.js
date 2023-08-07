fd697ec03b5e3e5f61b78e760991ce6af517157b
Fasih Muhammad <82199241+fasihmuhammadvirk@users.noreply.github.com>
2023-08-07 16:50:46 +0500

    initial

--- Practice Concepts /main.js
+++ Practice Concepts /main.js
@@ -12,70 +12,59 @@ var c = 8;
 	let a = 12; // locally scoped inside the block
 	const b = 13;
 
-    // if we changed tha value of c it will be changed globally 
-    var c = 14;
-    console.log(a ,b ,c ); // 12 13 14
+	// if we changed tha value of c it will be changed globally
+	var c = 14;
+	console.log(a, b, c); // 12 13 14
 }
 
 console.log(a, b, c); // 10 9  14
 
-
-
 // Hoisting Example
 
-
 var an_var;
-console.log(an_var)
-an_var = 100 // will get undefined cause the value is not defined but the variable is declared.
+console.log(an_var);
+an_var = 100; // will get undefined cause the value is not defined but the variable is declared.
 
-
-console.log(another_var)
+console.log(another_var);
 var another_var = 110; // same as above.
 
-
 let a_let_var;
-console.log(a_let_var )
+console.log(a_let_var);
 a_let_var = 100; // undefined
 
-// // expected and error so 
+// // expected and error so
 // console.log(another_let_var)
 // another_let_var = 'some value' // here we will get a ReferenceError unlike var
 
+// Loops on Array
 
-// Loops on Array 
+let an_array = [2, 3, 41, 435, 75, 3, 9, 0, 1];
 
-let an_array = [2,3,41,435,75,3,9,0,1]
-
-console.log("forEach Loop")
+console.log("forEach Loop");
 //square of the element in the array
-an_array.forEach(element => { // return value
-    console.log(element*element)
+an_array.forEach((element) => {
+	// return value
+	console.log(element * element);
 });
 
-console.log("forOf Loop") // return value 
-for (let item of an_array)
-{
-    console.log(item)
+console.log("forOf Loop"); // return value
+for (let item of an_array) {
+	console.log(item);
 }
 
-console.log("forin Loop") // return index
-for(let item in an_array)
-{
-    console.log(item)
+console.log("forin Loop"); // return index
+for (let item in an_array) {
+	console.log(item);
 }
 
+// finding the second largest value in the array
 
-// finding the second largest value in the array 
+const compare = (a, b) => {
+	return a - b;
+};
 
-const compare = (a,b)=>{
-    return a - b
-}
-
-second_larget_element_in_arr = an_array.sort(compare).reverse()[1]
-console.log(second_larget_element_in_arr)
-
-
-
+second_larget_element_in_arr = an_array.sort(compare).reverse()[1];
+console.log(second_larget_element_in_arr);
 
 // ** Array Methods ** //
 
@@ -109,11 +98,10 @@ let return_from_unshift = an_array.unshift(10);
 console.log(an_array, return_from_unshift); // expected output : 10, 2, 3, 4, 5, 7 ] 6
 
 let num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-console.log(num.length)
+console.log(num.length);
 // delete the element in the array and also does not change the length of the array
 delete num[0];
 console.log(num.length);
-
 
 // concat two or more array into a new one
 let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19];
@@ -121,7 +109,6 @@ let num_even_more = [211, 212, 213, 214, 415, 416, 417, 418, 419];
 let newArray = num.concat(num_more, num_even_more);
 console.log(newArray);
 console.log(num, num_more);
-
 
 // sort method
 let num1 = [551, 22, 3, 14, 5, 6, 7, 8, 229];
@@ -137,24 +124,18 @@ console.log(newNum);
 let newNum2 = num2.slice(3, 5);
 console.log(newNum2);
 
+// ** Object Methods ** //
 
-
-// ** Object Methods ** // 
-
-//this 
 const an_obj = {
+	firstname: " faish muhammad",
+	lastname: " virk",
+	greet: function greet() {
+		return "Hello";
+	},
 
-        firstname:' faish muhammad',
-        lastname:' virk',
-        greet: function greet(){
-            return "Hello"
-        },
+	greetwithname: function greetname() {
+		return this.greet() + this.firstname + this.lastname;
+	},
+};
 
-        greetwithname: function greetname(){
-             return this.greet() + this.firstname + this.lastname 
-        }
-
-
-    };
-
-console.log("The object is",an_obj.greetwithname());
+console.log("The object is", an_obj.greetwithname());
